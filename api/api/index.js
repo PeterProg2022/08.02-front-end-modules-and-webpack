90c02fc31d55a2e6c3074bea066aa40ec77d18c0
Peter Prog <peter.prog@yahoo.com>
2022-08-07 19:17:58 +0800

    api: installHandler

--- api/api/index.js
+++ api/api/index.js
@@ -5,12 +5,12 @@ const { installHandler } = require('./api_handler');
 
 const app = express();
 
-installHandler(app);
 
 const port = process.env.API_SERVER_PORT || 3000;
 
 (async function start() {
   try {
+    await installHandler(app);
     await connectToDb();
     app.listen(port, () => {
       console.log(`API server started on port ${port}`);
