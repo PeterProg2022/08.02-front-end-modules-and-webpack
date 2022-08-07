830e07abab9c296754a16b2b3d4455184b25743a
Peter Prog <peter.prog@yahoo.com>
2022-08-07 18:39:33 +0800

    api: local ok

--- api/api/api_handler.js
+++ api/api/api_handler.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server-express');
 
-const GraphQLDate = require('./graphql_date.js');
-const about = require('./about.js');
-const issue = require('./issue.js');
+const GraphQLDate = require('./graphql_date');
+const about = require('./about');
+const issue = require('./issue');
 
 const resolvers = {
   Query: {
@@ -19,7 +19,7 @@ const resolvers = {
 };
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync('api/schema.graphql', 'utf-8'),
   resolvers,
   formatError: (error) => {
     console.log(error);
@@ -27,9 +27,10 @@ const server = new ApolloServer({
   },
 });
 
-function installHandler(app) {
+async function installHandler(app) {
   const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
   console.log('CORS setting:', enableCors);
+  await server.start();
   server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 }
 
