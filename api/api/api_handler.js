95cd435a7ffab2669d659d8075ec3a42e7505208
Peter Prog <peter.prog@yahoo.com>
2022-08-07 19:35:10 +0800

    api: export

--- api/api/api_handler.js
+++ api/api/api_handler.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-require('dotenv').config();
-const { ApolloServer } = require('apollo-server-express');
+// const fs = require('fs');
+// require('dotenv').config();
+// const { ApolloServer } = require('apollo-server-express');
 
-const GraphQLDate = require('./graphql_date');
-const about = require('./about');
-const issue = require('./issue');
+// const GraphQLDate = require('./graphql_date');
+// const about = require('./about');
+// const issue = require('./issue');
 
-const resolvers = {
-  Query: {
-    about: about.getMessage,
-    issueList: issue.list,
-  },
-  Mutation: {
-    setAboutMessage: about.setMessage,
-    issueAdd: issue.add,
-  },
-  GraphQLDate,
-};
+// const resolvers = {
+//   Query: {
+//     about: about.getMessage,
+//     issueList: issue.list,
+//   },
+//   Mutation: {
+//     setAboutMessage: about.setMessage,
+//     issueAdd: issue.add,
+//   },
+//   GraphQLDate,
+// };
 
-const server = new ApolloServer({
-  typeDefs: fs.readFileSync('api/schema.graphql', 'utf-8'),
-  resolvers,
-  formatError: (error) => {
-    console.log(error);
-    return error;
-  },
-});
+// const server = new ApolloServer({
+//   typeDefs: fs.readFileSync('api/schema.graphql', 'utf-8'),
+//   resolvers,
+//   formatError: (error) => {
+//     console.log(error);
+//     return error;
+//   },
+// });
 
-async function installHandler(app) {
+async function installHandler(app, server) {
   const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
   console.log('CORS setting:', enableCors);
   await server.start();
--- api/api/index.js
+++ api/api/index.js
@@ -1,7 +1,39 @@
+//----------------------------
 require('dotenv').config();
 const express = require('express');
 const { connectToDb } = require('./db');
 const { installHandler } = require('./api_handler');
+
+//----------------------------
+const fs = require('fs');
+const { ApolloServer } = require('apollo-server-express');
+
+const GraphQLDate = require('./graphql_date');
+const about = require('./about');
+const issue = require('./issue');
+
+const resolvers = {
+  Query: {
+    about: about.getMessage,
+    issueList: issue.list,
+  },
+  Mutation: {
+    setAboutMessage: about.setMessage,
+    issueAdd: issue.add,
+  },
+  GraphQLDate,
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('api/schema.graphql', 'utf-8'),
+  resolvers,
+  formatError: (error) => {
+    console.log(error);
+    return error;
+  },
+});
+
+//----------------------------
 
 const port = process.env.API_SERVER_PORT || 3000;
 
@@ -19,4 +51,4 @@ const app = express();
   }
 }());
 
-export default app;
+export default server;
